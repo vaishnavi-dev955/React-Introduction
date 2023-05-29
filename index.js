4451b797bfe769d0f38023cb6eb1cdeca210d324
vaishnavi-dev955 <vaishnavimandala1998@gmail.com>
2023-05-29 19:08:57 +0000

    commit message

---
+++ index.js
@@ -0,0 +1,7 @@
+const Element = (
+  <div>
+    <h1 className="greeting">Hello World</h1>
+    <p>The Code is included in other file</p>
+  </div>
+);
+ReactDOM.render(Element, document.getElementById("root"));
